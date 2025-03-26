fa4186c26b12498e3e532c8064623c96058b6f5e
Enni <satya.sai.d.enni@accenture.com>
2025-03-26 13:04:24 +0530

    added popup for item

--- practice-project2/src/App.jsx
+++ practice-project2/src/App.jsx
@@ -1,29 +1,27 @@
-import { useState } from 'react'
-import './App.css'
-import Cards from './components/Cards'
-
+import { useState } from "react";
+import "./App.css";
+import Cards from "./components/Cards";
 
 function App() {
-
   return (
     <>
-    <div className='mainDiv'>
-      <div className='title'>Welcome to RCB</div>
-      <div className='cards'>
-      <Cards name="Virat" number="18"/>
-      <Cards name="Virat" number="18"/>
-      <Cards name="Virat" number="18"/>
-      <Cards name="Virat" number="18"/>
-      <Cards name="Virat" number="18"/>
-      <Cards name="Virat" number="18"/>
-      <Cards name="Virat" number="18"/>
-      <Cards name="Virat" number="18"/>
-      <Cards name="Virat" number="18"/>
-      
+      <div className="mainDiv">
+        <div className="title">Welcome to RCB</div>
+        <div className="cards">
+          <Cards name="Virat" number="18" /> 
+          <Cards name="Virat" number="18" />
+          <Cards name="Virat" number="18" />
+          <Cards name="Virat" number="18" />
+          <Cards name="Virat" number="18" />
+          <Cards name="Virat" number="18" />
+        </div>
+        <div className="buttonClass">
+          <button className="modalButton" >Next</button>
+    
+        </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- practice-project2/src/components/Cards.jsx
+++ practice-project2/src/components/Cards.jsx
@@ -1,16 +1,36 @@
-import React from "react";
-
+import React, { useState } from "react";
+// import "../App.css"
 function Cards(props) {
+  const [modal, setModal] = useState(false);
+  const showModal = () => {
+    setModal(true);
+  };
+  const closeModal = () => {
+    setModal(false);
+  };
   return (
-    // <div className="">
-    <div className="Card">
-        <img src="https://wallpaperaccess.com/full/2615131.jpg" />
-      <div>
-        {props.name} {props.number}
+    <div className="">
+      <div className="Card">
+        <img
+          src="https://wallpaperaccess.com/full/2615131.jpg"
+          style={{ height: "300px", width: "220px" }}
+        />
+        <div>
+          {props.name} {props.number}
+        </div>
+        <div className="buttonClass">
+          <button className="modalButton" onClick={showModal}>
+            Preview
+          </button>
+        </div>
       </div>
-      
+      {modal && (
+        <>
+          <div className="overlay" onClick={closeModal}></div>
+          <div className="Modal1">This is Preview of the content</div>
+        </>
+      )}
     </div>
-    // </div>
   );
 }
 
